3a85fcf215377695a0e42ba4e540d404638661a0
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2025-03-30 15:45:32 +0200

    improve not connected styling

--- src/components/ConnectionStatus.jsx
+++ src/components/ConnectionStatus.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function ConnectionStatus({ connected }) {
 	return (
 		!connected ? (
-			<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-				<div className="bg-gray-800 p-8 rounded-lg text-center text-white text-9xl border-4 border-red-600">
+			<div className="fixed inset-0 bg-black/55 flex items-center justify-center">
+				<div className="bg-gray-800 p-8 rounded-lg text-center text-white text-6xl border-4 border-red-600">
 					Nicht verbunden!
 				</div>
 			</div>
