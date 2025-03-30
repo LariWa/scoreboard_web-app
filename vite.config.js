5d629319fabe8e9634f99d26b36b0c417dd5d44c
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2025-03-30 19:13:28 +0200

    don't build files into asset folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
-  
+ 
+    build: {
+      assetsDir: '', // Place assets directly in the output directory
+      rollupOptions: {
+        output: {
+          assetFileNames: '[name][extname]', // Keep asset file names unchanged
+          entryFileNames: '[name].js', // Keep entry files at root
+          chunkFileNames: '[name].js', // Keep chunk files at root
+        },
+      },
+    },
+
 })
