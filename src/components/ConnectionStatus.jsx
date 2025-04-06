a92ee67f1c93d138b5f42a8d5b2c6514a981fa06
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2025-04-06 20:11:55 +0200

    add check if site is online

--- src/components/ConnectionStatus.jsx
+++ src/components/ConnectionStatus.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function ConnectionStatus({ connected }) {
+	
+function ConnectionStatus({ connected}) {
+	const [online, setOnline] = useState(navigator.onLine);
+
+	useEffect(() => {
+		const goOnline = () => setOnline(true);
+		const goOffline = () => setOnline(false);
+	
+		window.addEventListener("online", goOnline);
+		window.addEventListener("offline", goOffline);
+	
+		return () => {
+		  window.removeEventListener("online", goOnline);
+		  window.removeEventListener("offline", goOffline);
+		};
+	  }, []);
+
 	return (
-		!connected ? (
+		!connected || !online ? (
 			<div className="fixed inset-0 bg-black/55 flex items-center justify-center">
 				<div className="bg-gray-800 p-8 rounded-lg text-center text-white text-6xl border-4 border-red-600">
 					Nicht verbunden!
